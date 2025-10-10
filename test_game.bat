63c727c3f2f951f1c8ac83e20ec7737769051302
pooshpoosh595 <56774730+pooshpoosh595@users.noreply.github.com>
2025-10-10 17:33:01 -0400

    Delete test_game.bat

--- test_game.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Testing Sawnick Out of Love executable...
-echo.
-cd /d "%~dp0"
-.\dist\Sawnick_Out_of_Love.exe
-echo.
-echo Game has exited. Press any key to close this window.
-pause >nul
